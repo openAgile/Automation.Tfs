536b197a3d443ef99c6c98dc7dcba0ab473ec359
Laureano Remedi <lremedi@gmail.com>
2014-03-11 17:34:58 -0300

    Removing index.

--- merge.ps1
+++ merge.ps1
@@ -3,5 +3,5 @@ param(
     $workspace = ''
 )
 Write-Host "Getting TFSConfig full path"
-$path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse)[0].FullName
+$path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse).FullName
 & $path /out:$workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.all.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Tools.dll
