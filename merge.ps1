92b3d421ee6a8aa81d3d218fcc65a7dd0e352a85
Laureano Remedi <lremedi@gmail.com>
2014-03-11 17:30:08 -0300

    Fixing alias name.

--- merge.ps1
+++ merge.ps1
@@ -1,7 +1,7 @@
 param(
     [alias("ws")]
-    $ws = ''
+    $workspace = ''
 )
 Write-Host "Getting TFSConfig full path"
-$path = (Get-ChildItem -path $ws\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse)[0].FullName
-& $path /out:$ws\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.all.exe $ws\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.exe $ws\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $ws\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Tools.dll
+$path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse)[0].FullName
+& $path /out:$workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.all.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Tools.dll
