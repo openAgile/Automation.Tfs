b5f0fd935c9b4dcc448108d7b4756aad5f0b638e
Laureano Remedi <lremedi@gmail.com>
2014-03-13 16:27:28 -0300

    Fixing build scripts.

--- merge.ps1
+++ merge.ps1
@@ -4,4 +4,4 @@ param(
 )
 Write-Host "Getting TFSConfig full path"
 $path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse).FullName
-& $path /out:$workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.all.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Tools.dll
+& $path /out:$workspace\VersionOne.Tfs.Configure.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Configure.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $workspace\VersionOne.Tfs.Configure\bin\Debug\VersionOne.Tfs.Tools.dll
