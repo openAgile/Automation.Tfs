de351b79bb4fbf18d4897f72e8fe286facc3d7a5
Laureano Remedi <lremedi@gmail.com>
2014-03-14 17:39:52 -0300

    Fixing IL merging problems after renaming.

--- merge.ps1
+++ merge.ps1
@@ -4,4 +4,4 @@ param(
 )
 Write-Host "Getting TFSConfig full path"
 $path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse).FullName
-& $path /out:$workspace\Automation.Tfs.exe $workspace\Automation.Tfs.Console\bin\Debug\Automation.Tfs.exe $workspace\VersionOne.Tfs.Configure\bin\Debug\CLAP.dll $workspace\Automation.Tfs.Tools\bin\Debug\Automation.Tfs.Tools.dll
+& $path /out:$workspace\Automation.Tfs.exe $workspace\Automation.Tfs.Console\bin\Debug\Automation.Tfs.exe $workspace\Automation.Tfs.Console\bin\Debug\CLAP.dll $workspace\Automation.Tfs.Console\bin\Debug\Automation.Tfs.Tools.dll
