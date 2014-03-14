7a4bce9dd690dae54d691e0adda818d72bdb8cec
Laureano Remedi <lremedi@gmail.com>
2014-03-14 17:42:48 -0300

    Console output renaming.

--- merge.ps1
+++ merge.ps1
@@ -2,6 +2,6 @@ param(
     [alias("ws")]
     $workspace = ''
 )
-Write-Host "Getting TFSConfig full path"
+Write-Host "Getting ilmerge full path"
 $path = (Get-ChildItem -path $workspace\ -filter "ilmerge.exe" -erroraction silentlycontinue -recurse).FullName
 & $path /out:$workspace\Automation.Tfs.exe $workspace\Automation.Tfs.Console\bin\Debug\Automation.Tfs.exe $workspace\Automation.Tfs.Console\bin\Debug\CLAP.dll $workspace\Automation.Tfs.Console\bin\Debug\Automation.Tfs.Tools.dll
